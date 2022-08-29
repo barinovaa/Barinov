18e0e3ad7a50355d9e9edace90f662fa29894322
Barinov <Barinov-a-a@mail.ru>
2022-08-29 15:34:28 +0500

    Первый коммит

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int agg = 18;
+        if (agg > 18) {
+            System.out.println(" возраст совершеннолетия ещё не наступил");
+        }
+    }
+}
